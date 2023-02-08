8bb7b4d1d21c518c398d50f83937759a869e655d
Mitchell Dederer <mitchelldederer@gmail.com>
2023-02-08 15:52:25 +1100

    refactor: migrations

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,7 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
-mod m20230208_125420_create_posts;
+mod m20220101_000001_create_user_table;
+mod m20230208_125420_create_post_table;
 
 pub struct Migrator;
 
@@ -9,8 +9,8 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20230208_125420_create_posts::Migration),
+            Box::new(m20220101_000001_create_user_table::Migration),
+            Box::new(m20230208_125420_create_post_table::Migration),
         ]
     }
 }
